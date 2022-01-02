37838f03c492ed5c295c6608c97090253cea1531
micserv010821 <dotnet290320@gmail.com>
2022-01-02 20:18:10 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,10 @@
 class Calculator():
     def add(self, x, y):
         return x + y
+    def sub(self, x, y):
+        pass
+    def mul(self, x, y):
+        pass
+    def div(self, x, y):
+        # if y == 0 raise ZeroDivisionError
+        pass
