a1677099616ad8a038e976dde587c5803b88d23a
micserv010821 <dotnet290320@gmail.com>
2022-01-02 20:16:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def test_add_two_small_numbers():
     calc = Calculator()
     actual = calc.add(3, 4)
     expected = 7
-    assert [3,4] == [3,4, 5]
+    assert actual == expected
 
 # (venv) D:\python_projects\today_02Jan22\02.01.2022>pytest main.py
